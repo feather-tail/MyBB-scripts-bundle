31abe2e5fcbb01eab9eb4bdbc82bea6631ae00e0
Ilya Ryzhkov <imeliaslake@gmail.com>
2025-08-24 18:12:18 +0300

    fix bbcode-indent.js

--- js/bbcode-indent/bbcode-indent.js
+++ js/bbcode-indent/bbcode-indent.js
@@ -6,7 +6,6 @@
   const config = helpers.getConfig('bbcodeIndent', {});
   const state = helpers.register('bbcodeIndent', {});
 
-  const escapeRegExp = (s) => s.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
 
   function injectButton() {
     const ref = $(config.buttonAfterSelector);
@@ -33,22 +32,81 @@
   }
 
   function processIndent(container) {
-    const rxTag = new RegExp(escapeRegExp(config.bbcode), 'gi');
-    const spanHTML = `<span style="display:inline-block;margin-left:${config.marginLeft};"></span>`;
-
     if (!container.innerHTML.includes(config.bbcode)) return;
 
-    container.innerHTML = container.innerHTML.replace(
-      rxTag,
-      (match, offset, full) => {
-        const prefix = full
-          .slice(0, offset)
-          .replace(/<[^>]+>/g, '')
-          .replace(/\s|&nbsp;/g, '');
+    const tag = config.bbcode.toLowerCase();
+    const walker = document.createTreeWalker(container, NodeFilter.SHOW_TEXT);
+    const nodes = [];
 
-        return (prefix ? '<br>' : '') + spanHTML;
-      },
-    );
+    while (walker.nextNode()) {
+      const n = walker.currentNode;
+      if (n.nodeValue.toLowerCase().includes(tag)) {
+        nodes.push(n);
+      }
+    }
+
+    nodes.forEach((node) => {
+      let cur = node;
+
+      while (cur) {
+        const idx = cur.nodeValue.toLowerCase().indexOf(tag);
+        if (idx === -1) break;
+
+        const range = document.createRange();
+        range.setStart(container, 0);
+        range.setEnd(cur, idx);
+        const needBr = /\S/.test(range.toString().replace(/\u00a0/g, ''));
+
+        const after = cur.splitText(idx);
+        after.nodeValue = after.nodeValue.slice(config.bbcode.length);
+        let target;
+        if (after.nodeValue) {
+          target = after;
+        } else {
+          target = after.nextSibling;
+          after.remove();
+        }
+
+        if (needBr) {
+          cur.parentNode.insertBefore(createEl('br'), target);
+        }
+
+        applyIndent(target);
+
+        cur =
+          target && target.nodeType === Node.TEXT_NODE
+            ? target
+            : target
+            ? target.nextSibling
+            : null;
+      }
+    });
+
+    function applyIndent(node) {
+      if (!node) return;
+
+      if (node.nodeType === Node.TEXT_NODE) {
+        const span = createEl('span', {
+          style: `display:inline-block;margin-left:${config.marginLeft};`,
+        });
+        node.parentNode.insertBefore(span, node);
+        span.appendChild(node);
+        return;
+      }
+
+      if (node.nodeType === Node.ELEMENT_NODE) {
+        const display = window.getComputedStyle(node).display;
+        if (['block', 'flex', 'grid', 'table', 'list-item'].includes(display)) {
+          node.style.marginLeft = config.marginLeft;
+        } else {
+          const span = createEl('span', {
+            style: `display:inline-block;margin-left:${config.marginLeft};`,
+          });
+          node.parentNode.insertBefore(span, node);
+          span.appendChild(node);
+        }
+      }
+    }
   }
 
   function init() {
