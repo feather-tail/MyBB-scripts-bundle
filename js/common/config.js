55de82509f15f9bb5f1dc2400a0484eaa84ca329
Ilya Ryzhkov <imeliaslake@gmail.com>
2025-08-31 17:46:18 +0300

    test config

--- js/common/config.js
+++ js/common/config.js
@@ -243,7 +243,7 @@
       charset: 'windows-1251',
       awards: {
         enabled: true,
-        apiUrl: 'http://core.rusff.me/rusff.php',
+        apiUrl: 'https://core.rusff.me/rusff.php',
         method: 'POST',
         rpcMethod: 'awards/index',
         requestId: '1',
