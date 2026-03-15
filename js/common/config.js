1a9a984b751e1df91090454a8847586ce585cb23
Ilya Ryzhkov <88281523+feather-tail@users.noreply.github.com>
2026-03-15 13:41:47 +0300

    Update config.js

--- js/common/config.js
+++ js/common/config.js
@@ -91,7 +91,7 @@
         labels: { type: 'Тип операции', amount: 'Количество' },
         inputStyle: 'color:#111 !important',
       },
-      access: { allowedTopicIds: ['155', '233', '318', '393', '503'], allowedGroupIds: [1, 2] },
+      access: { allowedTopicIds: ['155', '233', '318', '393', '503', '624'], allowedGroupIds: [1, 2] },
       profileFieldKey: 'fld4',
       operations: [
         { title: 'Начисление', factor: 1, topics: 'all' },
